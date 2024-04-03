0168e21334bdd6c4c7841ed896f77f44fcf6ab55
user <user@DESKTOP-L8LR2AJ>
2024-04-03 17:13:35 +0900

    Add : main

---
+++ rpg game/src/RpgGame/Main.java
@@ -0,0 +1,11 @@
+package RpgGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameManager gameManager = new GameManager();
+			
+	}
+
+}
---
+++ rpg game/src/RpgGame/UnitManager.java
@@ -0,0 +1,5 @@
+package RpgGame;
+
+public class UnitManager {
+
+}
